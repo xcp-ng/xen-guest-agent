7c3a75493c30ab79520ae3237d199181bde1d261
Teddy Astie <teddy.astie@vates.tech>
2025-02-18 18:26:18 +0100

    Add Windows support for provider-memory
    Signed-off-by: Teddy Astie <teddy.astie@vates.tech>

--- providers/provider-memory/src/windows.rs
+++ providers/provider-memory/src/windows.rs
@@ -1,5 +1,19 @@
+use std::io;
+
+use windows::Win32::System::SystemInformation::{GlobalMemoryStatusEx, MEMORYSTATUSEX};
+
+use crate::MemorySource;
+
 #[derive(Default)]
 pub struct WindowsMemorySource;
+
+fn read_memstatus_ex() -> io::Result<MEMORYSTATUSEX> {
+    let mut mem_status = MEMORYSTATUSEX::default();
+    mem_status.dwLength = size_of_val(&mem_status) as u32;
+
+    unsafe { GlobalMemoryStatusEx(&mut mem_status).map_err(io::Error::other)? };
+    Ok(mem_status)
+}
 
 impl MemorySource for WindowsMemorySource {
     fn new() -> io::Result<Self> {
@@ -7,15 +21,10 @@ impl MemorySource for WindowsMemorySource {
     }
 
     fn get_total_kb(&mut self) -> io::Result<usize> {
-        Err(io::Error::new(
-            io::ErrorKind::Unsupported,
-            "no implementation for mem_total",
-        ))
+        Ok(read_memstatus_ex()?.ullTotalPhys as usize)
     }
+
     fn get_available_kb(&mut self) -> io::Result<usize> {
-        Err(io::Error::new(
-            io::ErrorKind::Unsupported,
-            "no implementation for mem_avail",
-        ))
+        Ok(read_memstatus_ex()?.ullAvailPhys as usize)
     }
 }
